52db39ccff717bfd8c2ef7512e7f6aab535158e2
Mike Murray <hellomikemurray@gmail.com>
2019-10-10 17:07:50 -0700

    feat: add default page component
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ imports/client/ui/layouts/DefaultPage.js
@@ -0,0 +1,85 @@
+import React from "react";
+import { Meteor } from "meteor/meteor";
+import { Button, makeStyles } from "@material-ui/core";
+import useAuth from "../hooks/useAuth";
+import useRouter from "../hooks/useRouter";
+import { Components } from "@reactioncommerce/reaction-components";
+import ShopLogo from "/imports/client/ui/components/ShopLogoWithData/ShopLogoWithData";
+import Logger from "/client/modules/logger";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh"
+  },
+  content: {
+    width: "100%",
+    maxWidth: 480
+  },
+  logo: {
+    display: "flex",
+    justifyContent: "center",
+    marginBottom: theme.spacing(3)
+  },
+  logoutButton: {
+    textAlign: "center"
+  }
+}));
+
+/**
+ * Displays a not-found or login
+ * @returns {React.ReactElement} React component
+ */
+function DefaultPage() {
+  const { isAdmin, isLoggedIn, isLoading, setLoggingOut } = useAuth();
+  const { location, match } = useRouter();
+  const classes = useStyles();
+
+  let content = <Components.Login />;
+
+  if (isLoading) {
+    return null;
+  }
+
+  // if (match.path !== location.path) {
+  //   content = <Components.NotFound title="Page not found" />;
+  // }
+
+  if (isLoggedIn && !isAdmin) {
+    // Logger.warn("Missing storefront home URL. Please set this from the shop settings panel so that customer users can be redirected to your storefront.");
+    content = (
+      <div className={classes.logoutButton}>
+        <Button
+          color="primary"
+          onClick={() => {
+            setLoggingOut(true);
+            Meteor.logout((error) => {
+              if (error) Logger.error(error);
+              setLoggingOut(false);
+            });
+          }}
+          variant="contained"
+        >
+          Logout
+        </Button>
+      </div>
+    );
+  }
+
+  return (
+    <div id="reactionAppContainer">
+      <div className={classes.root}>
+        <div className={classes.content}>
+          <div className={classes.logo}>
+            <ShopLogo linkTo="/" size={200} />
+          </div>
+          {content}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default DefaultPage;
