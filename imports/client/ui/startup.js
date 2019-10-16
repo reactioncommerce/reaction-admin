48d731f3a311730224db574fed0e07b499d99066
Mike Murray <hellomikemurray@gmail.com>
2019-10-16 15:26:01 -0700

    fix: subscribe to shop to fix various loading issues
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/client/ui/startup.js
+++ imports/client/ui/startup.js
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter, Route } from "react-router-dom";
-import createBrowserHistory from "history/createBrowserHistory";
 import { Meteor } from "meteor/meteor";
+import { Tracker } from "meteor/tracker";
 import { ThemeProvider } from "styled-components";
 import MuiThemeProvider from "@material-ui/core/styles/MuiThemeProvider";
 import { ApolloProvider } from "react-apollo";
-import i18next from "i18next";
 import { ComponentsProvider } from "@reactioncommerce/components-context";
 import { TranslationProvider } from "/imports/plugins/core/ui/client/providers";
 import initApollo from "/imports/plugins/core/graphql/lib/helpers/initApollo";
@@ -22,33 +21,38 @@ Meteor.startup(() => {
   loadRegisteredBlocks();
   loadRegisteredComponents();
 
-  // const primaryShopSub = Meteor.subscribe("PrimaryShop");
-  // const packageSub = Meteor.subscribe("Packages");
-
   const apolloClient = initApollo();
 
-  ReactDOM.render(
-    (
-      <ApolloProvider client={apolloClient}>
-        <BrowserRouter>
-          <TranslationProvider>
-            <ComponentsProvider value={appComponents}>
-              <ThemeProvider theme={theme}>
-                <MuiThemeProvider theme={defaultTheme}>
-                  <Route>
-                    {(routeProps) => (
-                      <RouterContext.Provider value={routeProps}>
-                        <App />
-                      </RouterContext.Provider>
-                    )}
-                  </Route>
-                </MuiThemeProvider>
-              </ThemeProvider>
-            </ComponentsProvider>
-          </TranslationProvider>
-        </BrowserRouter>
-      </ApolloProvider>
-    ),
-    getRootNode()
-  );
+  Tracker.autorun((computation) => {
+    const primaryShopSub = Meteor.subscribe("PrimaryShop");
+
+    if (primaryShopSub.ready()) {
+      ReactDOM.render(
+        (
+          <ApolloProvider client={apolloClient}>
+            <BrowserRouter>
+              <TranslationProvider>
+                <ComponentsProvider value={appComponents}>
+                  <ThemeProvider theme={theme}>
+                    <MuiThemeProvider theme={defaultTheme}>
+                      <Route>
+                        {(routeProps) => (
+                          <RouterContext.Provider value={routeProps}>
+                            <App />
+                          </RouterContext.Provider>
+                        )}
+                      </Route>
+                    </MuiThemeProvider>
+                  </ThemeProvider>
+                </ComponentsProvider>
+              </TranslationProvider>
+            </BrowserRouter>
+          </ApolloProvider>
+        ),
+        getRootNode()
+      );
+
+      computation.stop();
+    }
+  });
 });
