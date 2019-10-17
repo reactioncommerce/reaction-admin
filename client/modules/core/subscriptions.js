2c29b2c4ec26eaa2b40e031111ecefa47f2a17ae
Eric Dobbertin <eric@dairystatedesigns.com>
2019-10-17 09:59:35 -0500

    feat: cleaner subscription pattern
    Subscribe to Packages only where necessary.
    We're trying to get rid of it.
    
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

--- client/modules/core/subscriptions.js
+++ client/modules/core/subscriptions.js
@@ -1,8 +1,4 @@
-import { Meteor } from "meteor/meteor";
-import { Tracker } from "meteor/tracker";
 import { SubsManager } from "meteor/meteorhacks:subs-manager";
-import Reaction from "./main";
-import { getUserId } from "./helpers/utils";
 
 export const Subscriptions = {};
 
@@ -14,28 +10,8 @@ Subscriptions.Manager = new SubsManager();
  * General Subscriptions
  */
 
-Tracker.autorun(() => {
-  const userId = getUserId();
-  Subscriptions.Account = Subscriptions.Manager.subscribe("Accounts");
-  Subscriptions.UserProfile = Meteor.subscribe("UserProfile", userId);
-});
-
-// Primary shop subscription
-Subscriptions.PrimaryShop = Subscriptions.Manager.subscribe("PrimaryShop");
-
-// This Packages subscription is used for the Active shop's packages
-Subscriptions.Packages = Subscriptions.Manager.subscribe("Packages");
-
-Subscriptions.Groups = Subscriptions.Manager.subscribe("Groups");
-
 Subscriptions.BrandAssets = Subscriptions.Manager.subscribe("BrandAssets");
-
-Tracker.autorun(() => {
-  // Reload Packages sub if shopId changes
-  // We have a persistent subscription to the primary shop's packages,
-  // so don't refresh sub if we're updating to primaryShopId sub
-  const shopId = Reaction.getShopId();
-  if (shopId && shopId !== Reaction.getPrimaryShopId()) {
-    Subscriptions.Packages = Subscriptions.Manager.subscribe("Packages", shopId);
-  }
-});
+Subscriptions.Groups = Subscriptions.Manager.subscribe("Groups");
+Subscriptions.MyAccount = Subscriptions.Manager.subscribe("MyAccount");
+Subscriptions.PrimaryShop = Subscriptions.Manager.subscribe("PrimaryShop");
+Subscriptions.UserProfile = Subscriptions.Manager.subscribe("UserProfile");
