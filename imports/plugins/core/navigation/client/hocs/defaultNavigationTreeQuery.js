1628ad0623b6089c5f087224e3a886d76a24ba77
Mike Murray <hellomikemurray@gmail.com>
2019-11-05 13:14:04 -0800

    fix: gql query param
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/navigation/client/hocs/defaultNavigationTreeQuery.js
+++ imports/plugins/core/navigation/client/hocs/defaultNavigationTreeQuery.js
@@ -2,7 +2,7 @@ import gql from "graphql-tag";
 import { navigationTreeWith10LevelsFragment } from "./fragments";
 
 export default gql`
-  query defaultNavigationTreeQuery($id: ID!, $language: String!, shopId: ID!) {
+  query defaultNavigationTreeQuery($id: ID!, $language: String!, $shopId: ID!) {
     navigationTreeById(id: $id, language: $language, shopId: $shopId) {
       ...NavigationTreeWith10Levels
     }
