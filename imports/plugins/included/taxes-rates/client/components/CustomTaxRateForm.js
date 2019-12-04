c4372de888273585af93fd6874061231ab5a413b
Mike Murray <hellomikemurray@gmail.com>
2019-12-04 15:12:48 -0800

    feat: update form layout
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/taxes-rates/client/components/CustomTaxRateForm.js
+++ imports/plugins/included/taxes-rates/client/components/CustomTaxRateForm.js
@@ -5,10 +5,15 @@ import { useMutation } from "@apollo/react-hooks";
 import gql from "graphql-tag";
 import { useSnackbar } from "notistack";
 import SimpleSchema from "simpl-schema";
-import Button from "@reactioncommerce/catalyst/Button";
-import Grid from "@material-ui/core/Grid";
-import MenuItem from "@material-ui/core/MenuItem";
-import TextField from "@material-ui/core/TextField";
+import {
+  Box,
+  Button,
+  DialogTitle,
+  DialogActions,
+  DialogContent,
+  MenuItem,
+  TextField
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/styles";
 import muiOptions from "reacto-form/cjs/muiOptions";
 import useReactoForm from "reacto-form/cjs/useReactoForm";
@@ -93,7 +98,13 @@ export default function CustomTaxRateForm(props) {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const { enqueueSnackbar } = useSnackbar();
 
-  const { doc, onSuccess = () => {}, shopId, taxCodes } = props;
+  const {
+    doc,
+    onCancel = () => {},
+    onSuccess = () => {},
+    shopId,
+    taxCodes
+  } = props;
 
   const [createTaxRate] = useMutation(createTaxRateMutation, {
     ignoreResults: true,
@@ -225,84 +236,98 @@ export default function CustomTaxRateForm(props) {
   }
 
   return (
-    <div>
-      <TextField
-        className={classes.textField}
-        error={hasErrors(["rate"])}
-        fullWidth
-        helperText={getFirstErrorMessage(["rate"])}
-        label={i18next.t("admin.taxFormFields.rate")}
-        onKeyPress={(event) => {
-          if (event.key === "Enter") submitForm();
-        }}
-        placeholder={i18next.t("admin.taxFormFields.ratePlaceholder")}
-        {...getInputProps("rate", muiOptions)}
-      />
-      <p>{i18next.t("admin.taxFormHelpText")}</p>
-      <TextField
-        className={classes.textField}
-        error={hasErrors(["country"])}
-        fullWidth
-        helperText={getFirstErrorMessage(["country"])}
-        label={i18next.t("admin.taxFormFields.country")}
-        onKeyPress={(event) => {
-          if (event.key === "Enter") submitForm();
-        }}
-        select
-        {...getInputProps("country", muiOptions)}
-      >
-        <MenuItem key="_any" value="">
-          {i18next.t("admin.taxGrid.any")}
-        </MenuItem>
-        {CountryOptions.map((option) => (
-          <MenuItem key={option.value} value={option.value}>
-            {option.label}
+    <>
+      <DialogTitle>
+        {i18next.t("admin.shortcut.taxesLabel")}
+      </DialogTitle>
+      <DialogContent>
+        <TextField
+          className={classes.textField}
+          error={hasErrors(["rate"])}
+          fullWidth
+          helperText={getFirstErrorMessage(["rate"])}
+          label={i18next.t("admin.taxFormFields.rate")}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") submitForm();
+          }}
+          placeholder={i18next.t("admin.taxFormFields.ratePlaceholder")}
+          {...getInputProps("rate", muiOptions)}
+        />
+        <p>{i18next.t("admin.taxFormHelpText")}</p>
+        <TextField
+          className={classes.textField}
+          error={hasErrors(["country"])}
+          fullWidth
+          helperText={getFirstErrorMessage(["country"])}
+          label={i18next.t("admin.taxFormFields.country")}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") submitForm();
+          }}
+          select
+          {...getInputProps("country", muiOptions)}
+        >
+          <MenuItem key="_any" value="">
+            {i18next.t("admin.taxGrid.any")}
           </MenuItem>
-        ))}
-      </TextField>
-      <TextField
-        className={classes.textField}
-        error={hasErrors(["region"])}
-        fullWidth
-        helperText={getFirstErrorMessage(["region"])}
-        label={i18next.t("admin.taxFormFields.region")}
-        onKeyPress={(event) => {
-          if (event.key === "Enter") submitForm();
-        }}
-        {...getInputProps("region", muiOptions)}
-      />
-      <TextField
-        className={classes.textField}
-        error={hasErrors(["postal"])}
-        fullWidth
-        helperText={getFirstErrorMessage(["postal"])}
-        label={i18next.t("admin.taxFormFields.postal")}
-        onKeyPress={(event) => {
-          if (event.key === "Enter") submitForm();
-        }}
-        {...getInputProps("postal", muiOptions)}
-      />
-      {taxCodesField}
-      <Grid className={classes.rightAlignedGrid} item xs={12}>
-        {!!doc &&
-          <Button
-            className={classes.deleteButton}
-            color="primary"
-            isDisabled={isSubmitting}
-            onClick={() => {
-              deleteTaxRate({
-                variables: {
-                  input: {
-                    shopId,
-                    taxRateId: doc._id
+          {CountryOptions.map((option) => (
+            <MenuItem key={option.value} value={option.value}>
+              {option.label}
+            </MenuItem>
+          ))}
+        </TextField>
+        <TextField
+          className={classes.textField}
+          error={hasErrors(["region"])}
+          fullWidth
+          helperText={getFirstErrorMessage(["region"])}
+          label={i18next.t("admin.taxFormFields.region")}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") submitForm();
+          }}
+          {...getInputProps("region", muiOptions)}
+        />
+        <TextField
+          className={classes.textField}
+          error={hasErrors(["postal"])}
+          fullWidth
+          helperText={getFirstErrorMessage(["postal"])}
+          label={i18next.t("admin.taxFormFields.postal")}
+          onKeyPress={(event) => {
+            if (event.key === "Enter") submitForm();
+          }}
+          {...getInputProps("postal", muiOptions)}
+        />
+        {taxCodesField}
+      </DialogContent>
+      <DialogActions>
+        <Box flex={1}>
+          {!!doc &&
+            <Button
+              className={classes.deleteButton}
+              color="primary"
+              isDisabled={isSubmitting}
+              onClick={() => {
+                deleteTaxRate({
+                  variables: {
+                    input: {
+                      shopId,
+                      taxRateId: doc._id
+                    }
                   }
-                }
-              });
-            }}
-            variant="contained"
-          >
-            {i18next.t("app.delete")}
-          </Button>}
+                });
+              }}
+            >
+              {i18next.t("app.delete")}
+            </Button>
+          }
+        </Box>
+        <Button
+          color="primary"
+          isDisabled={isSubmitting}
+          onClick={onCancel}
+        >
+          {i18next.t("app.cancel")}
+        </Button>
         <Button
           color="primary"
           isDisabled={isSubmitting}
@@ -311,8 +336,8 @@ export default function CustomTaxRateForm(props) {
         >
           {i18next.t("app.save")}
         </Button>
-      </Grid>
-    </div>
+      </DialogActions>
+    </>
   );
 }
 
@@ -328,6 +353,10 @@ CustomTaxRateForm.propTypes = {
     region: PropTypes.string,
     taxCode: PropTypes.string
   }),
+  /**
+   * Function to call if the current form action should be canceled
+   */
+  onCancel: PropTypes.func,
   /**
    * Function to call after form is successfully submitted
    */
