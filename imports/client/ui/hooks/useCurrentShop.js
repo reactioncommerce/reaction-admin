283f549285cefee4dfafe76a659beb9492f0744e
Eric Dobbertin <eric@dairystatedesigns.com>
2019-12-18 17:43:08 -0600

    feat: fix and update Shipping Settings page
    Signed-off-by: Eric Dobbertin <eric@dairystatedesigns.com>

---
+++ imports/client/ui/hooks/useCurrentShop.js
@@ -0,0 +1,58 @@
+import { useLazyQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
+import useCurrentShopId from "./useCurrentShopId.js";
+
+const getShopQuery = gql`
+  query getShop($id: ID!) {
+    shop(id: $id) {
+      _id
+      brandAssets {
+        navbarBrandImageId
+      }
+      defaultParcelSize {
+        height
+        length
+        weight
+        width
+      }
+      language
+      name
+      shopLogoUrls {
+        primaryShopLogoUrl
+      }
+      storefrontUrls {
+        storefrontHomeUrl
+        storefrontLoginUrl
+        storefrontOrderUrl
+        storefrontOrdersUrl
+        storefrontAccountProfileUrl
+      }
+    }
+  }
+`;
+
+/**
+ * React Hook that gets the globally current shop
+ * @return {Object} Object with `shop` and `shopId` props
+ */
+export default function useCurrentShop() {
+  const [shopId] = useCurrentShopId();
+
+  const [getShop, { called, data, loading, refetch }] = useLazyQuery(getShopQuery, {
+    fetchPolicy: "network-only"
+  });
+
+  // Wait until we're sure we have a shop ID to call the query
+  if (shopId && !called) {
+    getShop({
+      variables: { id: shopId }
+    });
+  }
+
+  return {
+    isLoadingShop: loading,
+    refetchShop: refetch,
+    shop: data && data.shop,
+    shopId
+  };
+}
--- imports/plugins/included/shipping-rates/client/index.js
+++ imports/plugins/included/shipping-rates/client/index.js
@@ -1,1 +1,9 @@
-export { default as ShippingRatesSettings } from "../containers/ShippingRatesSettings";
+import { registerBlock } from "/imports/plugins/core/components/lib";
+import ShippingRatesSettings from "../containers/ShippingRatesSettings";
+
+registerBlock({
+  region: "ShippingSettings",
+  name: "ShippingRatesSettings",
+  component: ShippingRatesSettings,
+  priority: 1
+});
