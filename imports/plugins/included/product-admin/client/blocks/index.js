9a6cee11788e576b5f6b73670c9f2a84905fdcbd
Mike Murray <hellomikemurray@gmail.com>
2020-01-23 11:28:02 -0800

    refactor: disable non-gql refactored blocks
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/blocks/index.js
+++ imports/plugins/included/product-admin/client/blocks/index.js
@@ -39,91 +39,90 @@ registerBlock({
   region: "ProductDetailMain",
   name: "ProductDetailForm",
   component: ProductDetailForm,
-  hocs: [withProductForm],
   priority: 10
 });
 
 // Media gallery card and form
-registerBlock({
-  region: "ProductDetailMain",
-  name: "ProductMediaForm",
-  component: ProductMediaForm,
-  priority: 20
-});
+// registerBlock({
+//   region: "ProductDetailMain",
+//   name: "ProductMediaForm",
+//   component: ProductMediaForm,
+//   priority: 20
+// });
 
-registerBlock({
-  region: "ProductDetailMain",
-  name: "ProductSocialForm",
-  component: ProductSocialForm,
-  hocs: [withProductForm],
-  priority: 30
-});
+// registerBlock({
+//   region: "ProductDetailMain",
+//   name: "ProductSocialForm",
+//   component: ProductSocialForm,
+//   hocs: [withProductForm],
+//   priority: 30
+// });
 
-registerBlock({
-  region: "ProductDetailMain",
-  name: "ProductTagForm",
-  component: ProductTagForm,
-  priority: 40
-});
+// registerBlock({
+//   region: "ProductDetailMain",
+//   name: "ProductTagForm",
+//   component: ProductTagForm,
+//   priority: 40
+// });
 
-registerBlock({
-  region: "ProductDetailMain",
-  name: "ProductMetadataForm",
-  component: ProductMetadataForm,
-  hocs: [withProductForm],
-  priority: 50
-});
+// registerBlock({
+//   region: "ProductDetailMain",
+//   name: "ProductMetadataForm",
+//   component: ProductMetadataForm,
+//   hocs: [withProductForm],
+//   priority: 50
+// });
 
-registerBlock({
-  region: "ProductDetailMain",
-  name: "VariantTable",
-  component: VariantTable,
-  priority: 60
-});
+// registerBlock({
+//   region: "ProductDetailMain",
+//   name: "VariantTable",
+//   component: VariantTable,
+//   priority: 60
+// });
 
-registerBlock({
-  region: "VariantDetailSidebar",
-  name: "VariantList",
-  component: VariantList,
-  priority: 10
-});
+// registerBlock({
+//   region: "VariantDetailSidebar",
+//   name: "VariantList",
+//   component: VariantList,
+//   priority: 10
+// });
 
-// Header
-registerBlock({
-  region: "VariantDetailHeader",
-  name: "VariantHeader",
-  component: VariantHeader,
-  hocs: [withVariantForm],
-  priority: 10
-});
+// // Header
+// registerBlock({
+//   region: "VariantDetailHeader",
+//   name: "VariantHeader",
+//   component: VariantHeader,
+//   hocs: [withVariantForm],
+//   priority: 10
+// });
 
-registerBlock({
-  region: "VariantDetailMain",
-  name: "VariantDetailForm",
-  component: VariantDetailForm,
-  hocs: [withVariantForm],
-  priority: 10
-});
+// registerBlock({
+//   region: "VariantDetailMain",
+//   name: "VariantDetailForm",
+//   component: VariantDetailForm,
+//   hocs: [withVariantForm],
+//   priority: 10
+// });
 
-registerBlock({
-  region: "VariantDetailMain",
-  name: "VariantMediaForm",
-  component: VariantMediaForm,
-  hocs: [withVariantForm],
-  priority: 20
-});
+// registerBlock({
+//   region: "VariantDetailMain",
+//   name: "VariantMediaForm",
+//   component: VariantMediaForm,
+//   hocs: [withVariantForm],
+//   priority: 20
+// });
 
-registerBlock({
-  region: "VariantDetailMain",
-  name: "VariantTaxForm",
-  component: VariantTaxForm,
-  hocs: [withVariantForm],
-  priority: 30
-});
+// registerBlock({
+//   region: "VariantDetailMain",
+//   name: "VariantTaxForm",
+//   component: VariantTaxForm,
+//   hocs: [withVariantForm],
+//   priority: 30
+// });
 
-registerBlock({
-  region: "VariantDetailMain",
-  name: "OptionTable",
-  component: OptionTable,
-  priority: 40
-});
+// registerBlock({
+//   region: "VariantDetailMain",
+//   name: "OptionTable",
+//   component: OptionTable,
+//   priority: 40
+// });
