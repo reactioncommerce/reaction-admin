e7638ac2b2cf495afc7808a1c1c9d7000914edbb
Mike Murray <hellomikemurray@gmail.com>
2020-01-24 15:59:59 -0800

    feat: add update product mutation definition
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ imports/plugins/included/product-admin/client/hooks/UpdateProductMutation.js
@@ -0,0 +1,88 @@
+import gql from "graphql-tag";
+
+export default gql`
+  mutation updateProduct($input: UpdateProductInput!) {
+    updateProduct(input: $input) {
+      product {
+        _id
+        currentProductHash
+        description
+        isDeleted
+        isVisible
+        metaDescription
+        metafields {
+          key
+          value
+        }
+        originCountry
+        pageTitle
+        productType
+        publishedAt
+        publishedProductHash
+        shop {
+          _id
+        }
+        slug
+        socialMetadata {
+          message
+          service
+        }
+        supportedFulfillmentTypes
+        tagIds
+        title
+        updatedAt
+        vendor
+        variants {
+          _id
+          attributeLabel
+          barcode
+          height
+          index
+          isDeleted
+          isVisible
+          length
+          metafields {
+            key
+            value
+          }
+          minOrderQuantity
+          optionTitle
+          originCountry
+          shop {
+            _id
+          }
+          sku
+          title
+          updatedAt
+          weight
+          width
+          options {
+            _id
+            attributeLabel
+            barcode
+            height
+            index
+            isDeleted
+            isVisible
+            length
+            metafields {
+              key
+              value
+            }
+            minOrderQuantity
+            optionTitle
+            originCountry
+            shop {
+              _id
+            }
+            sku
+            title
+            updatedAt
+            weight
+            width
+          }
+        }
+      }
+    }
+  }
+`;
