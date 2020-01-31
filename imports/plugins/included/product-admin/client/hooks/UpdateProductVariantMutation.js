94426653ba96a73116af7edd277916fd3574b8f3
Mike Murray <hellomikemurray@gmail.com>
2020-01-30 19:20:10 -0800

    feat: add update variant mutation definition
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

---
+++ imports/plugins/included/product-admin/client/hooks/UpdateProductVariantMutation.js
@@ -0,0 +1,33 @@
+import gql from "graphql-tag";
+
+export default gql`
+  mutation updateProductVariant($input: UpdateProductVariantInput!) {
+    updateProductVariant(input: $input) {
+      variant {
+        _id
+        attributeLabel
+        barcode
+        height
+        index
+        isDeleted
+        isVisible
+        length
+        metafields {
+          key
+          value
+        }
+        minOrderQuantity
+        optionTitle
+        originCountry
+        shop {
+          _id
+        }
+        sku
+        title
+        updatedAt
+        weight
+        width
+      }
+    }
+  }
+`;
