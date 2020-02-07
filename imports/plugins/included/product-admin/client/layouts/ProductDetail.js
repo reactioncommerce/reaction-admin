591aa51fd520c6e40298b8b72bf3d30ba1786917
Mike Murray <hellomikemurray@gmail.com>
2020-02-06 19:51:11 -0800

    feat: update to a full width two column layout
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/layouts/ProductDetail.js
+++ imports/plugins/included/product-admin/client/layouts/ProductDetail.js
@@ -1,13 +1,31 @@
 import React, { Fragment } from "react";
 import PropTypes from "prop-types";
-import { Blocks, Components } from "@reactioncommerce/reaction-components";
+import { Blocks } from "@reactioncommerce/reaction-components";
 import withStyles from "@material-ui/core/styles/withStyles";
-import Grid from "@material-ui/core/Grid";
+import {
+  Box,
+  Container,
+  Divider
+} from "@material-ui/core";
 import { Switch, Route } from "react-router-dom";
+import ProductToolbar from "../components/ProductToolbar";
 
 const styles = (theme) => ({
   block: {
     marginBottom: theme.spacing(3)
+  },
+  sidebar: {
+    flex: "1 1 auto",
+    maxWidth: 330,
+    height: `calc(100vh - ${theme.mixins.toolbar.minHeight}px)`,
+    overflowY: "auto",
+    borderRight: `1px solid ${theme.palette.divider}`
+  },
+  content: {
+    flex: "1 1 auto",
+    height: `calc(100vh - ${theme.mixins.toolbar.minHeight}px)`,
+    overflowY: "auto",
+    paddingTop: theme.spacing(5)
   }
 });
 
@@ -21,48 +39,53 @@ function ProductDetail(props) {
 
   return (
     <Fragment>
-      {/* <Components.ProductPublish /> */}
-      <Grid container spacing={3}>
-        <Grid item sm={12}>
-          <Blocks region="ProductDetailHeader" blockProps={blockProps} />
-        </Grid>
-        <Grid item sm={4}>
-          <Blocks region="ProductDetailSidebar" blockProps={blockProps} />
-        </Grid>
-        <Grid item sm={8}>
-          <Switch>
-            <Route
-              path="/products/:handle/:variantId/:optionId?"
-              render={() => (
-                <Blocks region="VariantDetailMain" blockProps={blockProps}>
-                  {(blocks) =>
-                    blocks.map((block, index) => (
-                      <div className={classes.block} key={index}>
-                        {block}
-                      </div>
-                    ))
-                  }
-                </Blocks>
-              )}
-            />
-            <Route
-              path="/products/:handle/"
-              render={() => (
-                <Blocks region="ProductDetailMain" blockProps={blockProps}>
-                  {(blocks) =>
-                    blocks.map((block, index) => (
-                      <div className={classes.block} key={index}>
-                        {block}
-                      </div>
-                    ))
-                  }
-                </Blocks>
-              )}
-            />
-          </Switch>
+      <ProductToolbar />
+      <Box display="flex">
+        <div className={classes.sidebar}>
+          <Box padding={2}>
+            <Blocks region="ProductDetailHeader" blockProps={blockProps} />
+          </Box>
+          <Divider />
+          <Box padding={2}>
+            <Blocks region="ProductDetailSidebar" blockProps={blockProps} />
+          </Box>
+        </div>
 
-        </Grid>
-      </Grid>
+        <div className={classes.content}>
+          <Container maxWidth="md">
+            <Switch>
+              <Route
+                path="/products/:handle/:variantId/:optionId?"
+                render={() => (
+                  <Blocks region="VariantDetailMain" blockProps={blockProps}>
+                    {(blocks) =>
+                      blocks.map((block, index) => (
+                        <div className={classes.block} key={index}>
+                          {block}
+                        </div>
+                      ))
+                    }
+                  </Blocks>
+                )}
+              />
+              <Route
+                path="/products/:handle/"
+                render={() => (
+                  <Blocks region="ProductDetailMain" blockProps={blockProps}>
+                    {(blocks) =>
+                      blocks.map((block, index) => (
+                        <div className={classes.block} key={index}>
+                          {block}
+                        </div>
+                      ))
+                    }
+                  </Blocks>
+                )}
+              />
+            </Switch>
+          </Container>
+        </div>
+      </Box>
     </Fragment>
   );
 }
