fd391a27d6795ebbf785c90ead8a997bbea578f3
Will Lopez <will.lopez77@gmail.com>
2020-02-07 15:58:43 -0800

    refactor: wire up general settings
    Signed-off-by: Will Lopez <will.lopez77@gmail.com>

--- imports/plugins/core/dashboard/client/components/GeneralSettings.js
+++ imports/plugins/core/dashboard/client/components/GeneralSettings.js
@@ -1,15 +1,21 @@
 import React, { useState } from "react";
 import i18next from "i18next";
 import SimpleSchema from "simpl-schema";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
-import CardHeader from "@material-ui/core/CardHeader";
-import { makeStyles } from "@material-ui/styles";
-import { Box } from "@material-ui/core";
 import Button from "@reactioncommerce/catalyst/Button";
 import TextField from "@reactioncommerce/catalyst/TextField";
 import useReactoForm from "reacto-form/cjs/useReactoForm";
 import muiOptions from "reacto-form/cjs/muiOptions";
+import {
+  Box,
+  Card,
+  CardContent,
+  CardHeader,
+  ExpansionPanel,
+  ExpansionPanelSummary,
+  ExpansionPanelDetails,
+  makeStyles
+} from "@material-ui/core";
+import ChevronUp from "mdi-material-ui/ChevronUp";
 import useShopSettings from "../hooks/useShopSettings";
 
 const useStyles = makeStyles((theme) => ({
@@ -23,22 +29,31 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
-const formSchema = new SimpleSchema({
+const generalSettings = new SimpleSchema({
   name: {
     type: String,
     min: 1
   },
   emails: {
     type: String,
-    regEx: SimpleSchema.RegEx.Email
+    regEx: SimpleSchema.RegEx.Email,
+    optional: true
   },
   slug: {
     type: String,
     min: 1
+  },
+  description: {
+    type: String,
+    optional: true
+  },
+  keywords: {
+    type: String,
+    optional: true
   }
 });
 
-const validator = formSchema.getFormValidator();
+const validator = generalSettings.getFormValidator();
 
 /**
  * Shop settings form block component
@@ -47,7 +62,7 @@ const validator = formSchema.getFormValidator();
 function ShopSettings() {
   const classes = useStyles();
   const [isSubmitting, setIsSubmitting] = useState(false);
-  const { onUpdateShop } = useShopSettings();
+  const { onUpdateShop, refetch, shop } = useShopSettings();
 
   const {
     getFirstErrorMessage,
@@ -57,62 +72,93 @@ function ShopSettings() {
   } = useReactoForm({
     async onSubmit(formData) {
       setIsSubmitting(true);
-      await onUpdateShop(formSchema.clean(formData));
+      await onUpdateShop(generalSettings.clean(formData));
       setIsSubmitting(false);
+      refetch();
     },
     validator(formData) {
-      return validator(formSchema.clean(formData));
-    }
+      return validator(generalSettings.clean(formData));
+    },
+    value: shop
   });
 
   return (
     <Card className={classes.card}>
       <CardHeader title={i18next.t("admin.settings.general.label")} />
       <CardContent>
-        <form
-          onSubmit={(event) => {
-            event.preventDefault();
-            submitForm();
-          }}
-        >
-          <TextField
-            className={classes.textField}
-            error={hasErrors(["name"])}
-            fullWidth
-            helperText={getFirstErrorMessage(["name"])}
-            label={i18next.t("admin.settings.address.nameLabel")}
-            placeholder={i18next.t("admin.settings.address.namePlaceholder")}
-            {...getInputProps("name", muiOptions)}
-          />
-          <TextField
-            className={classes.textField}
-            error={hasErrors(["emails"])}
-            fullWidth
-            helperText={getFirstErrorMessage(["emails"])}
-            label={i18next.t("admin.settings.address.emailLabel")}
-            placeholder={i18next.t("admin.settings.address.emailPlaceholder")}
-            {...getInputProps("emails", muiOptions)}
-          />
-          <TextField
-            className={classes.textField}
-            error={hasErrors(["slug"])}
-            fullWidth
-            helperText={getFirstErrorMessage(["slug"])}
-            label={i18next.t("admin.settings.address.slugLabel")}
-            placeholder={i18next.t("admin.settings.address.slugPlaceholder")}
-            {...getInputProps("slug", muiOptions)}
-          />
-          <Box textAlign="right">
-            <Button
-              color="primary"
-              disabled={isSubmitting}
-              variant="contained"
-              type="submit"
+        <ExpansionPanel>
+          <ExpansionPanelSummary
+            expandIcon={<ChevronUp />}
+            aria-controls="panel1a-content"
+            id="panel1a-header"
+          >
+          Settings
+          </ExpansionPanelSummary>
+          <ExpansionPanelDetails>
+            <form
+              onSubmit={(event) => {
+                event.preventDefault();
+                submitForm();
+              }}
             >
-              {isSubmitting ? i18next.t("admin.settings.saveProcessing") : i18next.t("app.save")}
-            </Button>
-          </Box>
-        </form>
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["name"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["name"])}
+                label={i18next.t("admin.settings.address.nameLabel")}
+                placeholder={i18next.t("admin.settings.address.namePlaceholder")}
+                {...getInputProps("name", muiOptions)}
+              />
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["emails"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["emails"])}
+                label={i18next.t("admin.settings.address.emailLabel")}
+                placeholder={i18next.t("admin.settings.address.emailPlaceholder")}
+                {...getInputProps("emails", muiOptions)}
+              />
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["slug"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["slug"])}
+                label={i18next.t("admin.settings.address.slugLabel")}
+                placeholder={i18next.t("admin.settings.address.slugPlaceholder")}
+                {...getInputProps("slug", muiOptions)}
+              />
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["description"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["description"])}
+                label={i18next.t("admin.settings.address.descriptionLabel")}
+                placeholder={i18next.t("admin.settings.address.descriptionPlaceholder")}
+                {...getInputProps("description", muiOptions)}
+              />
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["keywords"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["keywords"])}
+                label={i18next.t("admin.settings.address.keywordsLabel")}
+                placeholder={i18next.t("admin.settings.address.keywordsPlaceholder")}
+                {...getInputProps("keywords", muiOptions)}
+              />
+              <Box textAlign="right">
+                <Button
+                  color="primary"
+                  disabled={isSubmitting}
+                  variant="contained"
+                  type="submit"
+                >
+                  {isSubmitting ? i18next.t("admin.settings.saveProcessing") : i18next.t("app.save")}
+                </Button>
+              </Box>
+            </form>
+          </ExpansionPanelDetails>
+        </ExpansionPanel>
       </CardContent>
     </Card>
   );
--- imports/plugins/core/dashboard/client/hooks/useShopSettings.js
+++ imports/plugins/core/dashboard/client/hooks/useShopSettings.js
@@ -1,10 +1,26 @@
-import { useState, useCallback, useMemo } from "react";
 import i18next from "i18next";
 import gql from "graphql-tag";
-import { useMutation, useQuery } from "@apollo/react-hooks";
+import { useMutation, useLazyQuery } from "@apollo/react-hooks";
 import useCurrentShopId from "/imports/client/ui/hooks/useCurrentShopId";
-import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
 import { useSnackbar } from "notistack";
+
+const SHOP_QUERY = gql`
+  query shopSettings($id: ID!) {
+    shop(id: $id) {
+      _id
+      addressBook {
+        company
+      }
+      description
+      emails {
+        address
+      }
+      keywords
+      name
+      slug
+    }
+  }
+`;
 
 const UPDATE_SHOP = gql`
   mutation updateShop($input: UpdateShopInput!) {
@@ -25,13 +41,27 @@ const UPDATE_SHOP = gql`
  * @returns {Object} The updated shop object
  */
 function useShopSettings(args = {}) {
-  const { enqueueSnackbar } = useSnackbar();
   const {
     shopId: providedShopId
   } = args;
-  const [updateShop] = useMutation(UPDATE_SHOP);
   const [currentShopId] = useCurrentShopId();
+  const [updateShop] = useMutation(UPDATE_SHOP);
   const shopId = providedShopId || currentShopId;
+  const { enqueueSnackbar } = useSnackbar();
+  const [fetchShop, { called, data: shopQueryResult, refetch }] = useLazyQuery(SHOP_QUERY);
+
+  if (shopId && !called) {
+    fetchShop({
+      variables: {
+        id: shopId
+      }
+    });
+  }
+
+  // apply transformation to emails field
+  if (shopQueryResult && shopQueryResult.shop && shopQueryResult.shop.emails) {
+    shopQueryResult.shop.emails = shopQueryResult.shop.emails[0].address;
+  }
 
   /**
    * @method onUpdateShop
@@ -66,6 +96,8 @@ function useShopSettings(args = {}) {
 
   return {
     onUpdateShop,
+    refetch,
+    shop: (shopQueryResult && shopQueryResult.shop) || {},
     shopId
   };
 }
