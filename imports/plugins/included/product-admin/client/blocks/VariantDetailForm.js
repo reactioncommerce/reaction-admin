657a450b510ef057e641eda15c718e2e57bd08df
Mike Murray <hellomikemurray@gmail.com>
2020-02-27 18:06:20 -0800

    refactor: simplify return value
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/blocks/VariantDetailForm.js
+++ imports/plugins/included/product-admin/client/blocks/VariantDetailForm.js
@@ -18,7 +18,6 @@ import CountryOptions from "@reactioncommerce/api-utils/CountryOptions.js";
 import TextField from "@reactioncommerce/catalyst/TextField";
 import useProduct from "../hooks/useProduct";
 
-
 const useStyles = makeStyles((theme) => ({
   card: {
     marginBottom: theme.spacing(2)
@@ -90,7 +89,6 @@ const VariantDetailForm = React.forwardRef((props, ref) => {
     option
   } = useProduct();
 
-  let content;
   const editingVariant = option || variant;
 
   const {
@@ -118,158 +116,155 @@ const VariantDetailForm = React.forwardRef((props, ref) => {
 
   const originCountryInputProps = getInputProps("originCountry", muiOptions);
 
-  if (product) {
-    content = (
-      <form
-        onSubmit={(event) => {
-          event.preventDefault();
-          submitForm();
-        }}
-      >
-        <TextField
-          className={classes.textField}
-          error={hasErrors(["attributeLabel"])}
-          fullWidth
-          helperText={getFirstErrorMessage(["attributeLabel"]) || i18next.t("admin.helpText.attributeLabel")}
-          label={i18next.t("admin.productVariant.attributeLabelLabel")}
-          placeholder={i18next.t("admin.productVariant.attributeLabelPlaceholder")}
-          {...getInputProps("attributeLabel", muiOptions)}
-        />
-
-        <TextField
-          className={classes.textField}
-          error={hasErrors(["optionTitle"])}
-          fullWidth
-          helperText={getFirstErrorMessage(["optionTitle"]) || i18next.t("admin.helpText.optionTitle")}
-          label={i18next.t("admin.productVariant.optionTitleLabel")}
-          placeholder={i18next.t("admin.productVariant.optionTitlePlaceholder")}
-          {...getInputProps("optionTitle", muiOptions)}
-        />
-
-        <TextField
-          className={classes.textField}
-          error={hasErrors(["title"])}
-          fullWidth
-          helperText={getFirstErrorMessage(["title"]) || i18next.t("admin.helpText.title")}
-          label={i18next.t("admin.productVariant.titleLabel")}
-          placeholder={i18next.t("admin.productVariant.titlePlaceholder")}
-          {...getInputProps("title", muiOptions)}
-        />
-
-        <TextField
-          className={classes.textField}
-          error={hasErrors(["originCountry"])}
-          fullWidth
-          helperText={getFirstErrorMessage(["originCountry"])}
-          label={i18next.t("productDetailEdit.originCountry")}
-          onKeyPress={(event) => {
-            if (event.key === "Enter") submitForm();
-          }}
-          select
-          {...originCountryInputProps}
-          value={originCountryInputProps.value || ""}
-        >
-          {CountryOptions.map((countryOption) => (
-            <MenuItem key={countryOption.value} value={countryOption.value}>
-              {countryOption.label}
-            </MenuItem>
-          ))}
-        </TextField>
-
-        <Grid container spacing={1}>
-          <Grid item sm={6}>
-            <TextField
-              className={classes.textField}
-              error={hasErrors(["price"])}
-              fullWidth
-              helperText={getFirstErrorMessage(["price"]) || i18next.t("admin.helpText.price")}
-              label={i18next.t("admin.productVariant.priceLabel")}
-              placeholder="0.00"
-              {...getInputProps("price", muiOptions)}
-            />
-          </Grid>
-          <Grid item sm={6}>
-            <TextField
-              className={classes.textField}
-              error={hasErrors(["compareAtPrice"])}
-              fullWidth
-              helperText={getFirstErrorMessage(["compareAtPrice"]) || i18next.t("admin.helpText.compareAtPrice")}
-              label={i18next.t("admin.productVariant.compareAtPriceLabel")}
-              placeholder="0.00"
-              {...getInputProps("compareAtPrice", muiOptions)}
-            />
-          </Grid>
-        </Grid>
-        <Divider />
-        <Grid container spacing={1}>
-          <Grid item sm={6}>
-            <TextField
-              className={classes.textField}
-              error={hasErrors(["width"])}
-              fullWidth
-              helperText={getFirstErrorMessage(["width"])}
-              label={i18next.t("admin.productVariant.width")}
-              placeholder="0"
-              {...getInputProps("width", muiOptions)}
-            />
-          </Grid>
-          <Grid item sm={6}>
-            <TextField
-              className={classes.textField}
-              error={hasErrors(["length"])}
-              fullWidth
-              helperText={getFirstErrorMessage(["length"])}
-              label={i18next.t("admin.productVariant.length")}
-              placeholder="0"
-              {...getInputProps("length", muiOptions)}
-            />
-          </Grid>
-        </Grid>
-
-        <Grid container spacing={1}>
-          <Grid item sm={6}>
-            <TextField
-              className={classes.textField}
-              error={hasErrors(["height"])}
-              fullWidth
-              helperText={getFirstErrorMessage(["height"])}
-              label={i18next.t("admin.productVariant.height")}
-              placeholder="0"
-              {...getInputProps("height", muiOptions)}
-            />
-          </Grid>
-          <Grid item sm={6}>
-            <TextField
-              className={classes.textField}
-              error={hasErrors(["weight"])}
-              fullWidth
-              helperText={getFirstErrorMessage(["v"])}
-              label={i18next.t("admin.productVariant.weight")}
-              placeholder="0"
-              {...getInputProps("weight", muiOptions)}
-            />
-          </Grid>
-        </Grid>
-
-        <Box textAlign="right">
-          <Button
-            color="primary"
-            disabled={!isDirty || isSubmitting}
-            variant="contained"
-            type="submit"
-          >
-            {i18next.t("app.save")}
-          </Button>
-        </Box>
-      </form>
-    );
-  }
-
   return (
     <Card className={classes.card} ref={ref}>
       <CardHeader title={i18next.t("admin.productAdmin.details")} />
       <CardContent>
-        {content}
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            submitForm();
+          }}
+        >
+          <TextField
+            className={classes.textField}
+            error={hasErrors(["attributeLabel"])}
+            fullWidth
+            helperText={getFirstErrorMessage(["attributeLabel"]) || i18next.t("admin.helpText.attributeLabel")}
+            label={i18next.t("admin.productVariant.attributeLabelLabel")}
+            placeholder={i18next.t("admin.productVariant.attributeLabelPlaceholder")}
+            {...getInputProps("attributeLabel", muiOptions)}
+          />
+
+          <TextField
+            className={classes.textField}
+            error={hasErrors(["optionTitle"])}
+            fullWidth
+            helperText={getFirstErrorMessage(["optionTitle"]) || i18next.t("admin.helpText.optionTitle")}
+            label={i18next.t("productVariant.optionTitle")}
+            placeholder={i18next.t("admin.productVariant.optionTitlePlaceholder")}
+            {...getInputProps("optionTitle", muiOptions)}
+          />
+
+          <TextField
+            className={classes.textField}
+            error={hasErrors(["title"])}
+            fullWidth
+            helperText={getFirstErrorMessage(["title"]) || i18next.t("admin.helpText.title")}
+            label={i18next.t("productVariant.title")}
+            placeholder={i18next.t("admin.productVariant.titlePlaceholder")}
+            {...getInputProps("title", muiOptions)}
+          />
+
+          <TextField
+            className={classes.textField}
+            error={hasErrors(["originCountry"])}
+            fullWidth
+            helperText={getFirstErrorMessage(["originCountry"])}
+            label={i18next.t("productDetailEdit.originCountry")}
+            onKeyPress={(event) => {
+              if (event.key === "Enter") submitForm();
+            }}
+            select
+            {...originCountryInputProps}
+            value={originCountryInputProps.value || ""}
+          >
+            {CountryOptions.map((countryOption) => (
+              <MenuItem key={countryOption.value} value={countryOption.value}>
+                {countryOption.label}
+              </MenuItem>
+            ))}
+          </TextField>
+
+          <Grid container spacing={1}>
+            <Grid item sm={6}>
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["price"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["price"]) || i18next.t("admin.helpText.price")}
+                label={i18next.t("productVariant.price")}
+                placeholder="0.00"
+                {...getInputProps("price", muiOptions)}
+              />
+            </Grid>
+            <Grid item sm={6}>
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["compareAtPrice"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["compareAtPrice"]) || i18next.t("admin.helpText.compareAtPrice")}
+                label={i18next.t("productVariant.compareAtPrice")}
+                placeholder="0.00"
+                {...getInputProps("compareAtPrice", muiOptions)}
+              />
+            </Grid>
+          </Grid>
+          <Box paddingBottom={4}>
+            <Divider />
+          </Box>
+          <Grid container spacing={1}>
+            <Grid item sm={6}>
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["width"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["width"])}
+                label={i18next.t("productVariant.width")}
+                placeholder="0"
+                {...getInputProps("width", muiOptions)}
+              />
+            </Grid>
+            <Grid item sm={6}>
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["length"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["length"])}
+                label={i18next.t("productVariant.length")}
+                placeholder="0"
+                {...getInputProps("length", muiOptions)}
+              />
+            </Grid>
+          </Grid>
+
+          <Grid container spacing={1}>
+            <Grid item sm={6}>
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["height"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["height"])}
+                label={i18next.t("productVariant.height")}
+                placeholder="0"
+                {...getInputProps("height", muiOptions)}
+              />
+            </Grid>
+            <Grid item sm={6}>
+              <TextField
+                className={classes.textField}
+                error={hasErrors(["weight"])}
+                fullWidth
+                helperText={getFirstErrorMessage(["v"])}
+                label={i18next.t("productVariant.weight")}
+                placeholder="0"
+                {...getInputProps("weight", muiOptions)}
+              />
+            </Grid>
+          </Grid>
+
+          <Box textAlign="right">
+            <Button
+              color="primary"
+              disabled={!product || !isDirty || isSubmitting}
+              isWaiting={isSubmitting}
+              type="submit"
+              variant="contained"
+            >
+              {i18next.t("app.saveChanges")}
+            </Button>
+          </Box>
+        </form>
       </CardContent>
     </Card>
   );
