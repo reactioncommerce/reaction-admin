0cb5d2badb71aa170109b3a738dbf3d16e4a866c
Mike Murray <hellomikemurray@gmail.com>
2020-02-28 17:23:23 -0800

    refactor: update route registration options
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/navigation/client/index.js
+++ imports/plugins/core/navigation/client/index.js
@@ -2,16 +2,16 @@
 import React from "react";
 import LinkIcon from "mdi-material-ui/LinkVariant";
 import { registerOperatorRoute } from "/imports/client/ui";
+import ContentViewFullLayout from "/imports/client/ui/layouts/ContentViewFullLayout";
 import NavigationDashboard from "./containers/navigationDashboardContainer";
-
 import "./containers";
 
 registerOperatorRoute({
   isNavigationLink: true,
   isSetting: false,
   priority: 50,
-  layoutComponent: null,
-  mainComponent: NavigationDashboard,
+  LayoutComponent: ContentViewFullLayout,
+  MainComponent: NavigationDashboard,
   path: "/navigation",
   // eslint-disable-next-line react/display-name
   SidebarIconComponent: (props) => <LinkIcon {...props} />,
