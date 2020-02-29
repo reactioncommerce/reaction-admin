71807299a4e0b0cfb912b17004c98e531768060d
Mike Murray <hellomikemurray@gmail.com>
2020-02-28 17:22:47 -0800

    refactor: convert to function component and use hooks
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/client/ui/layouts/Dashboard.js
+++ imports/client/ui/layouts/Dashboard.js
@@ -1,23 +1,18 @@
-import React, { Component } from "react";
+import React, { useMemo, useState } from "react";
 import PropTypes from "prop-types";
-import Helmet from "react-helmet";
-import { i18next } from "/client/api";
-import { compose } from "recompose";
-import withStyles from "@material-ui/core/styles/withStyles";
-import withWidth, { isWidthDown } from "@material-ui/core/withWidth";
 import { CustomPropTypes } from "@reactioncommerce/components/utils";
 import { withComponents } from "@reactioncommerce/components-context";
-import { Route, Switch } from "react-router";
-import { withRouter } from "react-router-dom";
+import { makeStyles } from "@material-ui/core";
 import PrimaryAppBar from "../components/PrimaryAppBar/PrimaryAppBar";
 import ProfileImageWithData from "../components/ProfileImageWithData";
 import Sidebar from "../components/Sidebar";
 import { operatorRoutes } from "../index";
 import { UIContext } from "../context/UIContext";
-import ContentViewFullLayout from "./ContentViewFullLayout";
-import ContentViewStandardLayout from "./ContentViewStandardLayout";
+import useOperatorRoutes from "../hooks/useOperatorRoutes";
+import useMediaQuery from "../hooks/useMediaQuery";
+import OperatorRoutes from "./OperatorRoutes";
 
-const styles = (theme) => ({
+const useStyles = makeStyles((theme) => ({
   "@global": {
     html: {
       // Remove the 10px fontSize from the html element as it affects fonts that rely on rem
@@ -30,150 +25,79 @@ const styles = (theme) => ({
   "leftSidebarOpen": {
     ...theme.mixins.leadingPaddingWhenPrimaryDrawerIsOpen
   }
-});
+}));
 
-class Dashboard extends Component {
-  static propTypes = {
-    classes: PropTypes.object,
-    components: PropTypes.shape({
-      IconHamburger: CustomPropTypes.component.isRequired
-    }),
-    location: PropTypes.object,
-    logout: PropTypes.func,
-    viewer: PropTypes.object,
-    width: PropTypes.string
+/**
+ * Main dashboard layout
+ * @param {Object} props Component props
+ * @returns {React.ReactElement} A react element representing the main dashboard
+ */
+function Dashboard(props) {
+  const { logout, viewer } = props;
+  const classes = useStyles();
+  const isMobile = useMediaQuery("mobile");
+  const routes = useOperatorRoutes({ group: "main" });
+  const [isDetailDrawerOpen, setDetailDrawerOpen] = useState(false);
+  const [isPrimarySidebarOpen, setPrimarySidebarOpen] = useState(true);
+
+  const onTogglePrimarySidebar = () => {
+    setPrimarySidebarOpen((prevValue) => !prevValue);
   };
 
-  constructor(props) {
-    super(props);
-
-    // State also contains the updater function so it will
-    // be passed down into the context provider
-    this.state = {
-      isDetailDrawerOpen: false,
-      isMobile: false,
-      isPrimarySidebarOpen: true,
-      onClosePrimarySidebar: this.onClosePrimarySidebar,
-      onTogglePrimarySidebar: this.onTogglePrimarySidebar,
-      onCloseDetailDrawer: this.onCloseDetailDrawer,
-      onToggleDetailDrawer: this.onToggleDetailDrawer
-    };
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    const { width, location } = this.props;
-    const isMobile = isWidthDown("sm", width);
-
-    if (prevState.isMobile !== isMobile) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({
-        isMobile
-      });
-    }
-
-    if (!isMobile && prevState.isPrimarySidebarOpen === false) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({
-        isPrimarySidebarOpen: true
-      });
-    }
-
-    // Close the detail drawer on route change
-    if (location.pathname !== prevProps.location.pathname) {
-      // eslint-disable-next-line react/no-did-update-set-state
-      this.setState({
-        isDetailDrawerOpen: false
-      });
-    }
-  }
-
-  onTogglePrimarySidebar = () => {
-    this.setState((state) => ({
-      isPrimarySidebarOpen: !state.isPrimarySidebarOpen
-    }));
+  const onToggleDetailDrawer = () => {
+    setDetailDrawerOpen((prevValue) => !prevValue);
   };
 
-  onToggleDetailDrawer = () => {
-    this.setState((state) => ({
-      isDetailDrawerOpen: !state.isDetailDrawerOpen
-    }));
+  const onCloseDetailDrawer = () => {
+    setDetailDrawerOpen(false);
   };
 
-  onCloseDetailDrawer = () => {
-    this.setState({ isDetailDrawerOpen: false });
+  const onClosePrimarySidebar = () => {
+    setPrimarySidebarOpen(false);
   };
 
-  onClosePrimarySidebar = () => {
-    this.setState({ isPrimarySidebarOpen: false });
-  };
+  const contextValue = useMemo(() => ({
+    isDetailDrawerOpen,
+    isMobile,
+    isPrimarySidebarOpen,
+    onClosePrimarySidebar,
+    onTogglePrimarySidebar,
+    onCloseDetailDrawer,
+    onToggleDetailDrawer,
+    setDetailDrawerOpen,
+    setPrimarySidebarOpen
+  }), [isDetailDrawerOpen, isMobile, isPrimarySidebarOpen]);
 
-  render() {
-    const { classes, logout, viewer, width } = this.props;
-    const { isDetailDrawerOpen, isPrimarySidebarOpen } = this.state;
-    const isMobile = isWidthDown("sm", width);
-
-    return (
-      <UIContext.Provider value={this.state}>
-        <div className={classes.container}>
-          <PrimaryAppBar>
-            <ProfileImageWithData logout={logout} size={40} viewer={viewer} />
-          </PrimaryAppBar>
-          <Sidebar
-            isMobile={isMobile}
-            isSidebarOpen={isPrimarySidebarOpen && !isDetailDrawerOpen}
-            setIsSidebarOpen={(value) => {
-              this.setState({ isPrimarySidebarOpen: value });
-            }}
-            onDrawerClose={this.state.onClosePrimarySidebar}
-            routes={operatorRoutes}
-          />
-          <Switch>
-            {
-              operatorRoutes.map((route) => (
-                <Route
-                  exact
-                  key={route.path}
-                  path={route.path}
-                  render={(props) => {
-                    const title = i18next.t(route.sidebarI18nLabel, { defaultValue: "Reaction Admin" });
-                    // If the layout component is explicitly null
-                    if (route.layoutComponent === null) {
-                      return (
-                        <ContentViewFullLayout
-                          isLeadingDrawerOpen={!isMobile}
-                          isTrailingDrawerOpen={isDetailDrawerOpen && !isMobile}
-                        >
-                          <Helmet title={title} />
-                          <route.mainComponent uiState={this.state} {...props} />
-                        </ContentViewFullLayout>
-                      );
-                    }
-
-                    const LayoutComponent = route.layoutComponent || ContentViewStandardLayout;
-
-                    return (
-                      <LayoutComponent
-                        isLeadingDrawerOpen={!isMobile}
-                        isTrailingDrawerOpen={isDetailDrawerOpen && !isMobile}
-                      >
-                        <Helmet title={title} />
-                        <route.mainComponent uiState={this.state} {...props} />
-                      </LayoutComponent>
-                    );
-                  }}
-                />
-              ))
-            }
-          </Switch>
-        </div>
-      </UIContext.Provider>
-    );
-  }
+  return (
+    <UIContext.Provider value={contextValue}>
+      <div className={classes.container}>
+        <PrimaryAppBar>
+          <ProfileImageWithData logout={logout} size={40} viewer={viewer} />
+        </PrimaryAppBar>
+        <Sidebar
+          isMobile={isMobile}
+          isSidebarOpen={isPrimarySidebarOpen}
+          setIsSidebarOpen={(value) => {
+            setPrimarySidebarOpen(value);
+          }}
+          onDrawerClose={onClosePrimarySidebar}
+          routes={operatorRoutes}
+        />
+        <OperatorRoutes routes={routes} />
+      </div>
+    </UIContext.Provider>
+  );
 }
 
-export default compose(
-  withComponents,
-  withRouter,
-  withWidth({ initialWidth: "md" }),
-  withStyles(styles, { name: "RuiDashboard" })
-)(Dashboard);
+Dashboard.propTypes = {
+  classes: PropTypes.object,
+  components: PropTypes.shape({
+    IconHamburger: CustomPropTypes.component.isRequired
+  }),
+  location: PropTypes.object,
+  logout: PropTypes.func,
+  viewer: PropTypes.object,
+  width: PropTypes.string
+};
+
+export default withComponents(Dashboard);
