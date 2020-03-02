c094011895399abf25581fa9a99d8386abd53461
Mike Murray <hellomikemurray@gmail.com>
2020-03-02 10:19:46 -0800

    refactor: use updated configuration
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/address/client/index.js
+++ imports/plugins/core/address/client/index.js
@@ -2,9 +2,8 @@ import { registerOperatorRoute } from "/imports/client/ui";
 import ShopAddressValidationSettings from "./containers/ShopAddressValidationSettings";
 
 registerOperatorRoute({
-  isNavigationLink: true,
-  isSetting: true,
-  mainComponent: ShopAddressValidationSettings,
+  group: "settings",
+  MainComponent: ShopAddressValidationSettings,
   path: "/address-validation-settings",
   priority: 900,
   sidebarI18nLabel: "addressValidation.title"
