5ecafa98b2a4d8a9499c7d2eb22305d0721f4a4d
Mike Murray <hellomikemurray@gmail.com>
2020-03-02 19:05:56 -0800

    fix: use no layout
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/core/navigation/client/index.js
+++ imports/plugins/core/navigation/client/index.js
@@ -2,14 +2,14 @@
 import React from "react";
 import LinkIcon from "mdi-material-ui/LinkVariant";
 import { registerOperatorRoute } from "/imports/client/ui";
-import ContentViewFullLayout from "/imports/client/ui/layouts/ContentViewFullLayout";
 import NavigationDashboard from "./containers/navigationDashboardContainer";
 import "./containers";
 
 registerOperatorRoute({
   group: "navigation",
   priority: 50,
-  LayoutComponent: ContentViewFullLayout,
+  // Use no layout. NavigationDashboard uses ContentViewPrimaryDetailLayout inside itself.
+  LayoutComponent: null,
   MainComponent: NavigationDashboard,
   path: "/navigation",
   // eslint-disable-next-line react/display-name
