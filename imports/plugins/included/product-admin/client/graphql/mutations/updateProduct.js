9ebd0b21725f67237b242e43595c2b71af217735
Mike Murray <hellomikemurray@gmail.com>
2020-03-10 10:07:44 -0700

    feat: use product with variants fragment
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/graphql/mutations/updateProduct.js
+++ imports/plugins/included/product-admin/client/graphql/mutations/updateProduct.js
@@ -1,11 +1,13 @@
 import gql from "graphql-tag";
+import Product from "../fragments/productWithVariants";
 
 export default gql`
   mutation updateProduct($input: UpdateProductInput!){
     updateProduct(input: $input){
       product {
-        isVisible
+        ...Product
       }
     }
-}
+  }
+  ${Product}
 `;
