fdb84e006aece30532b489006e9fd0d38bc657eb
Mike Murray <hellomikemurray@gmail.com>
2020-03-11 09:47:12 -0700

    feat: adjust padding
    Signed-off-by: Mike Murray <hellomikemurray@gmail.com>

--- imports/plugins/included/product-admin/client/layouts/ProductDetail.js
+++ imports/plugins/included/product-admin/client/layouts/ProductDetail.js
@@ -43,11 +43,14 @@ function ProductDetail(props) {
       AppBarComponent={<ProductToolbar />}
       PrimaryComponent={
         <>
-          <Box padding={2}>
+          <Box
+            paddingY={4}
+            paddingLeft={4}
+          >
             <Blocks region="ProductDetailHeader" blockProps={blockProps} />
           </Box>
           <Divider />
-          <Box padding={2}>
+          <Box paddingY={2}>
             <Blocks region="ProductDetailSidebar" blockProps={blockProps} />
           </Box>
         </>
