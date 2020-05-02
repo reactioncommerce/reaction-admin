90a290d28200335d3cbcc399073e227a010d84ad
Loan Laux <loan@outgrow.io>
2020-05-02 23:34:58 +0200

    chore: remove useless GQL mutations and imports
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- imports/plugins/core/accounts/client/graphql/mutations/archiveGroups.js
+++
@@ -1,9 +0,0 @@
-import gql from "graphql-tag";
-
-export default gql`
-  mutation createAccountGroup($input: CreateAccountGroupInput!) {
-    createAccountGroup(input: $input) {
-      group
-    }
-  }
-`;
