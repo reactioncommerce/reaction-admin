24dec1425dcad3d0bf27752f13f6fe836b6ed78b
Loan Laux <loan@outgrow.io>
2021-07-01 16:35:31 +0300

    chore: use enzyme-adapter-react-17
    Signed-off-by: Loan Laux <loan@outgrow.io>

--- imports/test-utils/setupJestTests.js
+++ imports/test-utils/setupJestTests.js
@@ -1,5 +1,5 @@
 const Enzyme = require("enzyme");
-const EnzymeAdapter = require("enzyme-adapter-react-16");
+const EnzymeAdapter = require("@wojtekmaj/enzyme-adapter-react-17");
 
 // Setup enzyme react adapter
 Enzyme.configure({ adapter: new EnzymeAdapter() });
